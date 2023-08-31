a21e6eceebf5bc60127a5d11c3548e779d28cc68
Ana Clara <anaclaraguimaraesremotto@gmail.com>
2023-08-31 11:22:23 -0300

    dicionario invertido

---
+++ inverte_dicionario.py
@@ -0,0 +1,17 @@
+def inverte(dic):
+    outro_dic = {}
+
+    for chave in dic:
+        valor = dic[chave]
+        outro_dic[valor] = chave
+    return outro_dic
+
+frutas = {'apple' : 'maçã', 'strawberry' : 'morango',
+          'watermelon' : 'melancia', 'orange' : 'laranja',
+          'passion fruit' : 'maracujá',
+          'peach' : 'pêssego', 'pineapple' : 'abacaxi',
+          'lemon' : 'limão', 'avocado' : 'abacate'}
+
+fruta_invertida = inverte(frutas)
+
+print(fruta_invertida)
