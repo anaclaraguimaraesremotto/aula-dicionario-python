77330a2411d1b6cbb7d1bf4a16dcab9d68de8a65
Ana Clara <anaclaraguimaraesremotto@gmail.com>
2023-08-31 11:21:10 -0300

    conta letras

---
+++ conta_letras.py
@@ -0,0 +1,15 @@
+contagem = {}
+
+frase = input("Informe o texto: ")
+
+#garante que a letra seja minuscula
+frase = frase.lower()
+
+for letra in frase:
+    if letra in contagem:
+        contagem[letra] = contagem[letra] + 1
+    else: 
+            contagem[letra] = 1
+
+for chave in contagem:
+    print(chave, "->", contagem[chave])
